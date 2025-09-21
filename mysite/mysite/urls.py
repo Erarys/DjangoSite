4f564cd3da61d6520b5c51bf42f689ddd47d75b7
Erarys <erarysabylkasym3@gmail.com@>
2025-09-22 00:32:36 +0500

    warning temporarys update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('cart/', include('cart.urls')),
 ]
-
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
