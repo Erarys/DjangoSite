4de895a3a2913f2fe9e03a16d90b5743dc495bdf
Erarys <erarysabylkasym3@gmail.com@>
2025-09-24 01:07:08 +0500

    new obj

--- mysite/myauth/models.py
+++ mysite/myauth/models.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
 class User(AbstractUser):
     email = models.EmailField(max_length=100, unique=True)
     username = None
-    USERNAME_FIELD = 'email'
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()   # вот тут!
