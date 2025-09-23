f391e912ac84500445e04137206a1187dea139fd
Erarys <erarysabylkasym3@gmail.com@>
2025-09-24 01:11:26 +0500

    also new superuser

--- mysite/myauth/models.py
+++ mysite/myauth/models.py
@@ -1,12 +1,38 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('Email обязателен'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Суперпользователь должен иметь is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Суперпользователь должен иметь is_superuser=True'))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     email = models.EmailField(max_length=100, unique=True)
-    username = None
+    username = None  # отключаем username
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = UserManager()   # вот тут!
+    objects = CustomUserManager()
