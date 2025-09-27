f3e655321e52200001a23d669f0e03a2a1e5decc
Erarys <erarysabylkasym3@gmail.com@>
2025-09-27 19:33:45 +0500

    new host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["dev.headloser.site"]
+ALLOWED_HOSTS = ["dev.headloser.site", "headloser.site"]
 
 # Application definition
 
